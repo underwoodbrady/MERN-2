29d37bd7bb5c6bd9b71f56ab6cc0132867fd2abe
Brady Underwood <underwood.brady@gmail.com>
2022-11-03 21:56:06 -0600

    testing

--- index.js
+++ index.js
@@ -27,35 +27,3 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-let io = require("socket.io")(httpsServ, {
-    cors: {
-        origin: "http://localhost:2000",
-        methods: ["GET", "POST"],
-    },
-});
-
-let serverCount = 0;
-
-let SOCKET_LIST = [];
-
-io.on("connection", (socket) => {
-    SOCKET_LIST.push(socket);
-
-    console.log("New client connected");
-
-    socket.emit("newValue", { count: serverCount });
-
-    socket.on("updateValue", (newVal) => {
-        serverCount = serverCount + newVal;
-        for (let i = 0; i < SOCKET_LIST.length; i++)
-            SOCKET_LIST[i].emit("newValue", { count: serverCount });
-    });
-
-    socket.on("disconnect", function () {
-        SOCKET_LIST.splice(socket, 1);
-    });
-});
-
-httpsServ.listen(PORT + 1, () =>
-    console.log(`Server running on port: ${PORT + 1} (https)`)
-);
