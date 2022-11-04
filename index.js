53643287e31b96fe6a9b9ee57ec9d2a50ec6374d
Brady Underwood <underwood.brady@gmail.com>
2022-11-03 22:14:13 -0600

    testing6

--- index.js
+++ index.js
@@ -23,10 +23,6 @@ app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
 app.use(express.static(path.join(__dirname, '...', 'build')));
 
-app.get('/', (req,res)=>{
-    res.send("hi");
-})
-
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
