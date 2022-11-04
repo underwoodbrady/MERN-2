21199d23af2bdf57653d734152fffbd78d5d88a5
Brady Underwood <underwood.brady@gmail.com>
2022-11-03 22:42:50 -0600

    cleanup

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
 const express = require("express"),
     app = express(),
-    https = require("https"),
-    fs = require("fs"),
     serv = require('http').Server(app),
     path = require('path');
 
-/* HTTPS Stuff
-const privateKey = fs.readFileSync("cert/localhost-key.pem", "utf8");
-const certificate = fs.readFileSync("cert/localhost.pem", "utf8");
-
-const credentials = {
-    key: privateKey,
-    cert: certificate,
-};
-let httpsServ = https.Server(credentials, app);
-/*End of HTTPS Stuff*/
 
 let PORT = process.env.PORT || 2020;
-
-//let index = require("./routes/index.js");
-
-//app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -30,11 +14,6 @@ app.get('/*', (req, res) => {
 
 serv.listen(PORT);
 
-/*
-httpsServ.listen(PORT + 1, () =>
-    console.log(`Server running on port: ${PORT + 1} (https)`)
-);
-*/
 let io = require("socket.io")(serv, {});
 
 let serverCount = 0;
